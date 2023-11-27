62b1f15d288b27b4545e4801208272c4ba9a53c4
jesutera <140918552+jesutera@users.noreply.github.com>
2023-11-26 19:56:23 -0500

    Para verificar el inicio de sesion

---
+++ validacion.js
@@ -0,0 +1,49 @@
+// Función para registrar y almacenar el ususarioen el localStorage 
+function guardarUsuario() {
+    //Captura los datos del formulario
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById("apellido").value;
+    var correo = document.getElementById("correo").value;
+    var contraseña = document.getElementById("contraseña").value;
+    var genero = document.getElementById("genero").value;
+
+    // Crea un objeto con los datos del usuario 
+    var ususario = {
+        nombre: nombre,
+        apellido: apellido,
+        correo: correo, 
+        contraseña: contraseña, 
+        genero: genero,
+    };
+
+    // Convierte el objeto en una cadena JSON
+    var usuarioJSON = JSON.stringify(usuario);
+
+    // Guarda los datos del usuario en el localStorage
+    localStorage.setItem('usuario', usuarioJSON);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
